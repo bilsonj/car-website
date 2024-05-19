1229af42f5da3e9cbdf391a427e148a5f4e4f971
jilson-j <jilsonjildas@gmail.com>
2024-05-19 20:14:34 +0530

    login uptated

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -2,10 +2,18 @@ import React, { useState } from "react";
 import "./nav.css";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
-import { Button } from "@mui/material";
+import { Button ,TextField,Box } from "@mui/material";
 import { Link } from "react-router-dom";
+import Modal from "react-modal";
 function Nav1() {
+
   const [mobile, setmobile] = useState(false);
+
+
+  const [value ,setvalue]=useState(false)
+
+
+
   return (
     <div>
       <nav className="navbar1">
@@ -23,8 +31,26 @@ function Nav1() {
           <Link to="/contacts1">
             <li className="contacts1">Contacts</li>
           </Link>
-          <li className="login1">Login</li>
+          <li className="login1"
+               onClick={()=>setvalue(true)}
+          >Login</li>
         </ul>
+           
+        <Modal className="modeloverall" isOpen={value} onRequestClose={()=>setvalue(true)} sx={{}}>
+              <Box className="modelcontiner">
+              <h1 className="fields">Enquire now</h1>
+              <TextField className="input" placeholder="name">
+                
+              </TextField>
+              <TextField className="input" placeholder="email">
+                
+                </TextField>
+                <Button className="modelbtn" sx={{ border: "2px solid #00ADB5", loadingPosition: "center" }}>Supmit</Button>
+              </Box>
+         
+            </Modal>
+
+           
         <Button className="mob-menu-icon" onClick={() => setmobile(!mobile)}>
           {mobile ? (
             <CloseIcon className="menu-close-color" />
